09386ea7362194db77611dfc80f297525c4476a4
ADMIN <osmyorkin@gmail.com>
2020-06-09 21:19:56 +0300

    Stage 1/4: Card anatomy

--- banking_system.py
+++ banking_system.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from random import sample
 
 
 class Card:
@@ -9,4 +9,26 @@ class Card:
 
     def __init__(self, _id):
         self.id = _id
-        self.number = '400000' + datetime.today().strftime('%Y%')
+        self.number = str(4000000000000000 + self.id)
+        self.pin = ''.join(map(str, sample(range(0, 9), 4)))
+
+
+class Account:
+    pass
+
+
+class Banking:
+    def __init__(self):
+        self.accounts = {}
+
+    def menu(self):
+        print('1. Create an account')
+        print('2. Log into account')
+        print('0. Exit')
+        self.menu_choice = int(input())
+
+    def create_account(self):
+        pass
+
+    def login(self):
+        pass
